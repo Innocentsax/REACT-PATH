54f5d5c032b6f59aed30d35557b0c3f1eaf58e48
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-10-25 15:57:42 +0100

    JavaScript review

--- 02-JS-REVIEW/script.js
+++ 02-JS-REVIEW/script.js
@@ -211,3 +211,12 @@ countWrong;
 
 const count = book.reviews.librarything.reviewsCount ?? "no data";
 count;
+
+// Optional Chaining
+function getTotalReviewCount(book) {
+  const goodreads = book.reviews?.goodreads?.reviewsCount;
+  const librarything = book.reviews?.librarything?.reviewsCount ?? 0;
+  return goodreads + librarything;
+}
+
+console.log(getTotalReviewCount(book));
